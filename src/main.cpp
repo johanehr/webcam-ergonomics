95e435c3a31407bcd25ce51384600483f5f3ad96
Johan Ehrenfors <johan.ehrenfors@hotmail.com>
2019-07-12 17:07:23 +0200

    Read JSON configuration file

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,27 @@
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <opencv2/opencv.hpp>
+#include "json.hpp"
+
+using namespace nlohmann;
 using namespace cv;
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-    Mat image;
-    image = imread( argv[1], 1 );
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
+
+    // read a JSON file
+    std::ifstream f("config/settings.json");
+    json settings;
+    f >> settings;
+
+    double ipd = settings["ipd"];
+    double alert_time = settings["alert_time"];
+
+    // Print current settings
+    std::cout << "Interpupillary distance: " << ipd*1000 << " mm\n";
+    std::cout << "Neutral position: (" << settings["neutral"][0] << ", " << settings["neutral"][1] << ", " << settings["neutral"][2] << ") m\n";
+    std::cout << "Time before alerted: " << alert_time << " s\n";
+
     return 0;
 }
