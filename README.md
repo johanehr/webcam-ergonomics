3895ec655addfff9340f225a0ed2d114ce3bfd75
Johan Ehrenfors <johan.ehrenfors@hotmail.com>
2019-08-12 16:34:01 +0200

    Alert beep period + finishing touches

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # webcam-ergonomics
-This is a computer vision project with the goal of estimating head position relative to the webcam, based on the assumption that the user mostly looks straight ahead. The constant distance between the eyes then allows for an estimate of distance from the webcam, as well as a general angle. If the head position strays outside a "safe-zone" around a neutral, ergonomically desired position for too long, a warning can be displayed, prompting the user to correct their posture. This neutral position can be adjusted on the fly.
+This is a computer vision project with the goal of estimating head position relative to the webcam, based on the assumption that the user mostly looks straight ahead. The constant distance between the eyes then allows for an estimate of distance from the webcam, as well as a general angle. If the head position strays outside a "safe-zone" around a neutral, ergonomically desired position for too long, a warning sound will be played, prompting the user to correct their posture. This neutral position and other settings can be adjusted by editing the settings.json file.
+
+Note that the focal length ("f" in settings.json) can be roughly estimated as follows:
+
+f = cot(a/2)w/2
+
+f: focal length
+a: horizontal field of view
+w: horizontal resolution (printed once in terminal)
+
+The eye detection used does not seem to work very well for slim eyes - the author included. This should be fixed for use outside a simple proof of concept.
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <opencv2/opencv.hpp>
 #include "json.hpp"
 
+#include <opencv2/opencv.hpp>
 #include "opencv2/objdetect.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
@@ -27,10 +28,19 @@ class ErgonomicsChecker {
     double neutral_radius;
     int num_received = 0;
     std::chrono::time_point<std::chrono::high_resolution_clock> last_OK_time = std::chrono::high_resolution_clock::now();
+    std::chrono::time_point<std::chrono::high_resolution_clock> last_alert = std::chrono::high_resolution_clock::now();
 
   public:
     ErgonomicsChecker(){
       readJsonSettings("config/settings.json");
+    }
+
+    double getAlertTime(){
+      return alert_time;
+    }
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> getLastOKTime(){
+      return last_OK_time;
     }
 
     void readJsonSettings(String file_path){
@@ -74,29 +84,63 @@ class ErgonomicsChecker {
       filtered_position[2] = z_filtered / TRAILING_AVG_LOCATIONS;
     }
 
-    void checkErgonomics(){
+    bool checkErgonomics(){
+
       // Compare current and neutral position, is it sufficiently close to neutral position?
       double distance = sqrt(
         pow(filtered_position[0]-neutral_position[0],2)
         +pow(filtered_position[1]-neutral_position[1],2)
         +pow(filtered_position[2]-neutral_position[2],2));
 
+      bool good_posture = false;
       // If inside, record OK alert_time
       if (distance <= neutral_radius){
         last_OK_time = std::chrono::high_resolution_clock::now();
+        good_posture = true;
       }
 
       // If time since OK exceeds alert_time, alert the user
       double time_since_OK = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - last_OK_time).count()/1000.0;
-      std::cout << "Time since OK: " << time_since_OK << "\n";
-      if (time_since_OK > alert_time) {
-        alertUser();
-      }
+      alertUser(alert_time - time_since_OK);
+
+      return good_posture;
     }
 
-    void alertUser(){
-      // TODO: Make this occur at increasing frequency or similar. Now beep-beep-beep depending on loop rate -> too often!
-      std::cout << "\a";
+    // countdown is in seconds. Values less than zero warrant an auditory alert
+    void alertUser(double countdown){
+
+      if (std::abs(countdown) < 0.05){
+        // Initial beep
+        std::cout << "\a";
+        last_alert = std::chrono::high_resolution_clock::now();
+      }
+
+      else if (countdown < 0){
+
+        double time_since_last_alert = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - last_alert).count()/1000.0;
+        double beep_period;
+
+        if (countdown < -60.0){
+          // Bad posture for a long time (60+s), beep every 1 second
+          beep_period = 1.0;
+
+        }
+        else if (countdown < -30.0){
+          // Bad posture for a medium time (30-60s), beep every 5 s
+          beep_period = 5.0;
+        }
+        else{
+          // Bad posture for a short amount of time (0-30s), beep every 10 s
+          beep_period = 10.0;
+        }
+
+        if (time_since_last_alert > beep_period){
+          std::cout << "\a";
+          last_alert = std::chrono::high_resolution_clock::now();
+        }
+
+
+      }
     }
 
 };
@@ -116,7 +160,8 @@ class LocationDetector {
     Point eye2_center = Point( 0, 0 );
     Point face_center = Point( 0, 0 );
     Mat frame;
-    // TODO: Add camera calibration
+    bool showResolutionOnce = false; // used to only show webcam resolution once
+
 
   public:
     LocationDetector() { // Constructor with parameters
@@ -164,6 +209,11 @@ class LocationDetector {
     int captureAndProcessImage() {
       cap >> frame;
 
+      if (!showResolutionOnce){
+        std::cout << "Webcam resolution: " << frame.cols << "x" << frame.rows << " px\n";
+        showResolutionOnce = true;
+      }
+
       Mat resized_frame (cvRound(frame.rows / downscale_factor), cvRound( frame.cols / downscale_factor), frame.type());
       resize(frame, resized_frame, resized_frame.size());
 
@@ -197,44 +247,87 @@ class LocationDetector {
           eye2_center.x = (faces[0].x + eyes[1].x + eyes[1].width/2)*downscale_factor;
           eye2_center.y = (faces[0].y + eyes[1].y + eyes[1].height/2)*downscale_factor;
 
-          std::cout << "Detected both eyes!\n";
-          return 2;
+          return 2; // Found face with 2 eyes
         }
         else{
-          std::cout << "eyes.size() == " << eyes.size() << "\n";
-          return 1;
+          return 1; // Found face, but not eyes
         }
       }
-      else{
-        std::cout << "faces.size() == " << faces.size() << "\n";
-      }
-      std::cout << "WARNING: Lost sight of eyes!\n";
+
+      // Lost sight of face completely
       return 0;
 
     }
 
-    void showLiveFeed(int detection_state){
+    void showLiveFeed(int detection_state, double countdown){
+
       int radius_eye = frame.cols/20;
       int radius_face = frame.cols/4;
 
+      Scalar white = Scalar( 255, 255, 255 );
+      Scalar orange = Scalar( 0, 128, 255 );
+      Scalar red = Scalar( 0, 0, 255 );
+
       // Mark in white if newly found, red if old detection
       if (detection_state == 2){ // Found both face and eyes
-        circle( frame, face_center, radius_face, Scalar( 255, 255, 255 ), 2 );
-        circle( frame, eye1_center, radius_eye, Scalar( 255, 255, 255 ), 1 );
-        circle( frame, eye2_center, radius_eye, Scalar( 255, 255, 255 ), 1 );
+        circle( frame, face_center, radius_face, white, 2 );
+        circle( frame, eye1_center, radius_eye, white, 1 );
+        circle( frame, eye2_center, radius_eye, white, 1 );
       }
       else if (detection_state == 1){ // Found only face
-        circle( frame, face_center, radius_face, Scalar( 255, 255, 255 ), 2 );
-        circle( frame, eye1_center, radius_eye, Scalar( 0, 0, 255 ), 1 );
-        circle( frame, eye2_center, radius_eye, Scalar( 0, 0, 255 ), 1 );
+        circle( frame, face_center, radius_face, white, 2 );
+        circle( frame, eye1_center, radius_eye, red, 1 );
+        circle( frame, eye2_center, radius_eye, red, 1 );
       }
       else { // Did not find at all
-        circle( frame, face_center, radius_face, Scalar( 0, 0, 255 ), 2 );
-        circle( frame, eye1_center, radius_eye, Scalar( 0, 0, 255 ), 1 );
-        circle( frame, eye2_center, radius_eye, Scalar( 0, 0, 255 ), 1 );
+        circle( frame, face_center, radius_face, red, 2 );
+        circle( frame, eye1_center, radius_eye, red, 1 );
+        circle( frame, eye2_center, radius_eye, red, 1 );
       }
 
-      // TODO: Add location (x,y,z) in meters??? Need to be move function call.
+      // Format decimals for presentation
+      std::stringstream stream;
+      stream << std::fixed << std::setprecision(2) << xCoord;
+      std::string xCoord_str = stream.str();
+      stream.str(std::string());
+
+      stream << std::fixed << std::setprecision(2) << yCoord;
+      std::string yCoord_str = stream.str();
+      stream.str(std::string());
+
+      stream << std::fixed << std::setprecision(2) << zCoord;
+      std::string zCoord_str = stream.str();
+      stream.str(std::string());
+
+      stream << std::fixed << std::setprecision(1) << countdown;
+      std::string countdown_str = stream.str();
+      stream.str(std::string());
+
+      std::string pos_text = "POSITION: (" + xCoord_str + ", " + yCoord_str + ", " + zCoord_str + ")";
+      std::string countdown_text = "COUNTDOWN: " + countdown_str + "s";
+
+      Scalar font_color;
+      Scalar font_countdown_color;
+
+      if (countdown > 9.0){
+        font_color = white;
+        font_countdown_color = white;
+      }
+      else if (countdown > 5.0){
+        font_color = red;
+        font_countdown_color = white;
+      }
+      else if (countdown > 0.0){
+        font_color = red;
+        font_countdown_color = orange;
+      }
+      else {
+        font_color = red;
+        font_countdown_color = red;
+      }
+
+      putText( frame, pos_text, Point(frame.cols/20, frame.cols/20), FONT_HERSHEY_SIMPLEX, 0.5, font_color );
+      putText( frame, countdown_text, Point(frame.cols/20, frame.cols/10), FONT_HERSHEY_SIMPLEX, 0.5, font_countdown_color );
 
       imshow( "webcam-ergonomics LIVE FEED", frame );
     }
@@ -252,8 +345,6 @@ class LocationDetector {
 
       xCoord = (x_avg - w/2.0)*zCoord/focal_length;
       yCoord = (y_avg - h/2.0)*zCoord/focal_length;
-
-      std::cout << "Position: (" << xCoord << ", " << yCoord << ", " << zCoord << ")\n";
     }
 };
 
@@ -286,19 +377,31 @@ int main(int argc, char** argv )
       locDet.calculateLocation();
       ergCheck.addNewLocation(locDet.xCoord, locDet.yCoord, locDet.zCoord);
       ergCheck.calcFilteredLocation();
-      ergCheck.checkErgonomics();
     }
 
+    // Regardless of whether location detected, use latest valid data to check ergo
+    bool good_posture = ergCheck.checkErgonomics();
+
     if (live_feed){
-      locDet.showLiveFeed(detection_state);
-      //locDet.showLiveFeed(detection_success, x, y, z, time_NOT_OK);
+
+      double seconds_since_OK = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - ergCheck.getLastOKTime()).count()/1000.0;
+      locDet.showLiveFeed(detection_state, ergCheck.getAlertTime() - seconds_since_OK);
     }
 
     auto t_end = std::chrono::high_resolution_clock::now();
     double elapsedTime = std::chrono::duration<double, std::milli>(t_end-t_start).count();
-    std::cout << "One loop took: "<<elapsedTime<<" ms.\n";
 
-    if( waitKey(10) == 27 ) break; // stop upon pressing ESC key
+    std::string posture_text;
+    if (good_posture){
+      posture_text = "GOOD";
+    }
+    else {
+      posture_text = "POOR";
+    }
+    std::cout << "\rPosture: " << posture_text << " --- script running at: ~"<<(int)(1000.0/elapsedTime)<< " Hz   " << std::flush;
+
+    if( waitKey(10) == 27 ) break; // stop upon pressing ESC key when preview is in focus
+
   }
   return 0;
 }
